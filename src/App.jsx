371d8dd83bf63bb320c9c7a7717728af0a635103
venkat <srivenkatsairaj13@gmail.com>
2022-07-21 01:20:54 +0530

    testing GA

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>
+        Testing <code>/dist</code> folder
+      </p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
